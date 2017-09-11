f1eb64975e68a75518713ac239190a2887b4bacc
Tan Xiaoyan <tanxiaoyan@renyibang.com>
2017-09-11 13:51:42 +0800

    增加ListUtils和ScreenUtils

---
+++ androidutils2/src/main/java/com/txy/androidutils/ListUtils.java
@@ -0,0 +1,36 @@
+package com.txy.androidutils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Apple on 17/9/11.
+ */
+
+public class ListUtils {
+
+    public static <T> boolean isEmpty(List<T> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static <T> int indexOfObj(List<T> list, T t) {
+        if (!isEmpty(list)) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) == t) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static <T> int size(List<T> list) {
+        return list == null ? 0 : list.size();
+    }
+
+    public static <T> List<T> fromObj2List(T t) {
+        List<T> list = new ArrayList<>();
+        list.add(t);
+        return list;
+    }
+}
---
+++ androidutils2/src/main/java/com/txy/androidutils/ScreenUtils.java
@@ -0,0 +1,31 @@
+package com.txy.androidutils;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Apple on 17/9/11.
+ */
+
+public class ScreenUtils {
+
+    public static int dp2px(Context context, int dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return (int) (density * dp + 0.5);
+    }
+
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    public int getStatusBarHeight(Context context) {
+        int statusBarHeight = -1;
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) statusBarHeight = context.getResources().getDimensionPixelOffset(resId);
+        return statusBarHeight;
+    }
+
+    public View getView(int layoutId, Context context) {
+        return View.inflate(context, layoutId, null);
+    }
+}
